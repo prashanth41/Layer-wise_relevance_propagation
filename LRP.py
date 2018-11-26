18d503b30e122b83874179300c7443de928633cb
prashanth <prashanthkanduri66@gmail.com>
2018-11-26 02:47:44 -0500

    Finished implementing epsilon LRP

---
+++ LRP.py
@@ -0,0 +1,152 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import warnings
+warnings.filterwarnings('ignore')
+
+import tempfile, sys, os
+sys.path.insert(0, os.path.abspath('..'))
+
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+
+# Download and import MNIST data
+tmp_dir = tempfile.gettempdir()
+mnist = input_data.read_data_sets(tmp_dir, one_hot=True)
+
+
+
+
+
+# Parameters
+learning_rate = 0.005
+num_steps = 2000
+batch_size = 128
+
+# Network Parameters
+n_hidden_1 = 256 # 1st layer number of neurons
+n_hidden_2 = 256 # 2nd layer number of neurons
+num_input = 784 # MNIST data input (img shape: 28*28)
+num_classes = 10 # MNIST total classes (0-9 digits)
+
+# tf Graph input
+X = tf.placeholder("float", [None, num_input])
+Y = tf.placeholder("float", [None, num_classes])
+
+# Store layers weight & bias
+weights = {
+    'h1': tf.Variable(tf.random_normal([num_input, n_hidden_1], mean=0.0, stddev=0.05)),
+    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2], mean=0.0, stddev=0.05)),
+    'out': tf.Variable(tf.random_normal([n_hidden_2, num_classes], mean=0.0, stddev=0.05))
+}
+biases = {
+    'b1': tf.Variable(tf.zeros([n_hidden_1])),
+    'b2': tf.Variable(tf.zeros([n_hidden_2])),
+    'out': tf.Variable(tf.zeros([num_classes]))
+}
+
+print("DONE")
+
+
+
+# Create and train model
+def model(x, act=tf.nn.relu):  # < different activation functions lead to different explanations
+    layer_1 = act(tf.add(tf.matmul(x, weights['h1']), biases['b1']))
+    layer_2 = act(tf.add(tf.matmul(layer_1, weights['h2']), biases['b2']))
+    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    return out_layer
+
+# Construct model
+logits = model(X)
+
+# Define loss and optimizer
+loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+    logits=logits, labels=Y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+train_op = optimizer.minimize(loss_op)
+
+# Evaluate model (with test logits, for dropout to be disabled)
+correct_pred = tf.equal(tf.argmax(logits, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+# Initialize the variables (i.e. assign their default value)
+init = tf.global_variables_initializer()
+
+# Train
+def input_transform (x): 
+    return (x - 0.5) *  2
+
+sess = tf.Session()
+
+# Run the initializer
+sess.run(init)
+
+for step in range(1, num_steps+1):
+    batch_x, batch_y = mnist.train.next_batch(batch_size)
+    batch_x = input_transform(batch_x)
+    # Run optimization op (backprop)
+    sess.run(train_op, feed_dict={X: batch_x, Y: batch_y})
+    if step % 100 == 0 or step == 1:
+        # Calculate batch loss and accuracy
+        loss, acc = sess.run([loss_op, accuracy], feed_dict={X: batch_x,
+                                                             Y: batch_y})
+        print("Step " + str(step) + ", Minibatch Loss= " + \
+              "{:.4f}".format(loss) + ", Training Accuracy= " + \
+              "{:.3f}".format(acc))
+
+print("Done")
+
+# Calculate accuracy for MNIST test images
+test_x = input_transform(mnist.test.images)
+test_y = mnist.test.labels
+
+print("Test accuracy:", \
+    sess.run(accuracy, feed_dict={X: test_x, Y: test_y}))
+
+
+print("DONE")
+
+
+
+# Import DeepExplain
+from methods import DeepExplain
+from utils import plot, plt
+import warnings
+warnings.filterwarnings('ignore')
+
+# Define the input to be tested
+test_idx = 89
+xi = test_x[[test_idx]]
+yi = test_y[test_idx] 
+
+# Create a DeepExplain context. 
+# IMPORTANT: the network must be created within this context.
+# In this example we have trained the network before, so we call `model(X)` to 
+# recreate the network graph using the same weights that have been already trained.
+with DeepExplain(session=sess) as de:
+    logits = model(X)
+    # We run `explain()` several time to compare different attribution methods
+    attributions = {
+        # Gradient-based
+        #'Saliency maps':        de.explain('saliency', logits * yi, X, xi),
+#         'Gradient * Input':     de.explain('grad*input', logits * yi, X, xi),
+#         'Integrated Gradients': de.explain('intgrad', logits * yi, X, xi),
+        'Epsilon-LRP':          de.explain('elrp', logits * yi, X, xi)
+        #'DeepLIFT (Rescale)':   de.explain('deeplift', logits * yi, X, xi),
+        #Perturbation-based
+        #'_Occlusion [1x1]':      de.explain('occlusion', logits * yi, X, xi),
+        #'_Occlusion [3x3]':      de.explain('occlusion', logits * yi, X, xi, window_shape=(3,))
+    }
+    print ('Done')
+
+# Plot attributions
+n_cols = len(attributions) + 1
+fig, axes = plt.subplots(nrows=1, ncols=n_cols, figsize=(3*n_cols, 3))
+plot(xi.reshape(28, 28), cmap='Greys', axis=axes[0]).set_title('Original')
+print(yi)
+for i in range(len(yi)):
+    if yi[i]==float(1):
+        print("test output : ",i)
+for i, method_name in enumerate(sorted(attributions.keys())):
+    plt.show(plot(attributions[method_name].reshape(28,28), xi = xi.reshape(28, 28), axis=axes[1+i]).set_title(method_name))
---
+++ methods.py
@@ -0,0 +1,250 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import sys
+import numpy as np
+from skimage.util import view_as_windows
+import warnings
+import tensorflow as tf
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import nn_grad, math_grad
+from collections import OrderedDict
+
+SUPPORTED_ACTIVATIONS = [
+    'Relu', 'Elu', 'Sigmoid', 'Tanh', 'Softplus'
+]
+
+UNSUPPORTED_ACTIVATIONS = [
+    'CRelu', 'Relu6', 'Softsign'
+]
+
+_ENABLED_METHOD_CLASS = None
+_GRAD_OVERRIDE_CHECKFLAG = 0
+
+
+# -----------------------------------------------------------------------------
+# UTILITY FUNCTIONS
+# -----------------------------------------------------------------------------
+
+
+def activation(type):
+    """
+    Returns Tensorflow's activation op, given its type
+    :param type: string
+    :return: op
+    """
+    if type not in SUPPORTED_ACTIVATIONS:
+        warnings.warn('Activation function (%s) not supported' % type)
+    f = getattr(tf.nn, type.lower())
+    return f
+
+
+def original_grad(op, grad):
+    """
+    Return original Tensorflow gradient for an op
+    :param op: op
+    :param grad: Tensor
+    :return: Tensor
+    """
+    if op.type not in SUPPORTED_ACTIVATIONS:
+        warnings.warn('Activation function (%s) not supported' % op.type)
+    opname = '_%sGrad' % op.type
+    if hasattr(nn_grad, opname):
+        f = getattr(nn_grad, opname)
+    else:
+        f = getattr(math_grad, opname)
+    return f(op, grad)
+
+
+# -----------------------------------------------------------------------------
+# ATTRIBUTION METHODS BASE CLASSES
+# -----------------------------------------------------------------------------
+
+
+class AttributionMethod(object):
+    """
+    Attribution method base class
+    """
+    def __init__(self, T, X, xs, session, keras_learning_phase=None):
+        self.T = T
+        self.X = X
+        self.xs = xs
+        self.session = session
+        self.keras_learning_phase = keras_learning_phase
+        self.has_multiple_inputs = type(self.X) is list or type(self.X) is tuple
+        print ('Model with multiple inputs: ', self.has_multiple_inputs)
+
+    def session_run(self, T, xs):
+        feed_dict = {}
+        if self.has_multiple_inputs:
+            if len(xs) != len(self.X):
+                raise RuntimeError('List of input tensors and input data have different lengths (%s and %s)'
+                                   % (str(len(xs)), str(len(self.X))))
+            for k, v in zip(self.X, xs):
+                feed_dict[k] = v
+        else:
+            feed_dict[self.X] = xs
+
+        if self.keras_learning_phase is not None:
+            feed_dict[self.keras_learning_phase] = 0
+        return self.session.run(T, feed_dict)
+
+    def _set_check_baseline(self):
+        if self.baseline is None:
+            if self.has_multiple_inputs:
+                self.baseline = [np.zeros((1,) + xi.shape[1:]) for xi in self.xs]
+            else:
+                self.baseline = np.zeros((1,) + self.xs.shape[1:])
+
+        else:
+            if self.has_multiple_inputs:
+                for i, xi in enumerate(self.xs):
+                    if self.baseline[i].shape == self.xs[i].shape[1:]:
+                        self.baseline[i] = np.expand_dims(self.baseline[i], 0)
+                    else:
+                        raise RuntimeError('Baseline shape %s does not match expected shape %s'
+                                           % (self.baseline[i].shape, self.xs[i].shape[1:]))
+            else:
+                if self.baseline.shape == self.xs.shape[1:]:
+                    self.baseline = np.expand_dims(self.baseline, 0)
+                else:
+                    raise RuntimeError('Baseline shape %s does not match expected shape %s'
+                                       % (self.baseline.shape, self.xs.shape[1:]))
+
+
+class GradientBasedMethod(AttributionMethod):
+    """
+    Base class for gradient-based attribution methods
+    """
+    def get_symbolic_attribution(self):
+        return tf.gradients(self.T, self.X)
+
+    def run(self):
+        attributions = self.get_symbolic_attribution()
+        results =  self.session_run(attributions, self.xs)
+        return results[0] if not self.has_multiple_inputs else results
+
+    @classmethod
+    def nonlinearity_grad_override(cls, op, grad):
+        return original_grad(op, grad)
+
+
+class EpsilonLRP(GradientBasedMethod):
+    eps = None
+
+    def __init__(self, T, X, xs, session, keras_learning_phase, epsilon=1e-4):
+        super(EpsilonLRP, self).__init__(T, X, xs, session, keras_learning_phase)
+        assert epsilon > 0.0, 'LRP epsilon must be greater than zero'
+        global eps
+        eps = epsilon
+
+    def get_symbolic_attribution(self):
+        return [g * x for g, x in zip(
+            tf.gradients(self.T, self.X),
+            self.X if self.has_multiple_inputs else [self.X])]
+
+    @classmethod
+    def nonlinearity_grad_override(cls, op, grad):
+        output = op.outputs[0]
+        input = op.inputs[0]
+        return grad * output / (input + eps *
+                                tf.where(input >= 0, tf.ones_like(input), -1 * tf.ones_like(input)))
+
+
+# # -----------------------------------------------------------------------------
+# # END ATTRIBUTION METHODS
+# # -----------------------------------------------------------------------------
+
+
+attribution_methods = OrderedDict({
+
+    'elrp': (EpsilonLRP, 4)
+})
+
+
+
+@ops.RegisterGradient("DeepExplainGrad")
+def deepexplain_grad(op, grad):
+    global _ENABLED_METHOD_CLASS, _GRAD_OVERRIDE_CHECKFLAG
+    _GRAD_OVERRIDE_CHECKFLAG = 1
+    if _ENABLED_METHOD_CLASS is not None \
+            and issubclass(_ENABLED_METHOD_CLASS, GradientBasedMethod):
+        return _ENABLED_METHOD_CLASS.nonlinearity_grad_override(op, grad)
+    else:
+        return original_grad(op, grad)
+
+
+class DeepExplain(object):
+
+    def __init__(self, graph=None, session=tf.get_default_session()):
+        self.method = None
+        self.batch_size = None
+        self.session = session
+        self.graph = session.graph if graph is None else graph
+        self.graph_context = self.graph.as_default()
+        self.override_context = self.graph.gradient_override_map(self.get_override_map())
+        self.keras_phase_placeholder = None
+        self.context_on = False
+        if self.session is None:
+            raise RuntimeError('DeepExplain: could not retrieve a session. Use DeepExplain(session=your_session).')
+
+    def __enter__(self):
+        # Override gradient of all ops created in context
+        self.graph_context.__enter__()
+        self.override_context.__enter__()
+        self.context_on = True
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.graph_context.__exit__(type, value, traceback)
+        self.override_context.__exit__(type, value, traceback)
+        self.context_on = False
+
+    def explain(self, method, T, X, xs, **kwargs):
+        if not self.context_on:
+            raise RuntimeError('Explain can be called only within a DeepExplain context.')
+        global _ENABLED_METHOD_CLASS, _GRAD_OVERRIDE_CHECKFLAG
+        self.method = method
+        if self.method in attribution_methods:
+            method_class, method_flag = attribution_methods[self.method]
+        else:
+            raise RuntimeError('Method must be in %s' % list(attribution_methods.keys()))
+        print('DeepExplain: running "%s" explanation method (%d)' % (self.method, method_flag))
+        self._check_ops()
+        _GRAD_OVERRIDE_CHECKFLAG = 0
+
+        _ENABLED_METHOD_CLASS = method_class
+        method = _ENABLED_METHOD_CLASS(T, X, xs, self.session, self.keras_phase_placeholder, **kwargs)
+        result = method.run()
+        if issubclass(_ENABLED_METHOD_CLASS, GradientBasedMethod) and _GRAD_OVERRIDE_CHECKFLAG == 0:
+            warnings.warn('DeepExplain detected you are trying to use an attribution method that requires '
+                          'gradient override but the original gradient was used instead. You might have forgot to '
+                          '(re)create your graph within the DeepExlain context. Results are not reliable!')
+        _ENABLED_METHOD_CLASS = None
+        _GRAD_OVERRIDE_CHECKFLAG = 0
+        self.keras_phase_placeholder = None
+        return result
+
+    @staticmethod
+    def get_override_map():
+        return dict((a, 'DeepExplainGrad') for a in SUPPORTED_ACTIVATIONS)
+
+    def _check_ops(self):
+        """
+        Heuristically check if any op is in the list of unsupported activation functions.
+        This does not cover all cases where explanation methods would fail, and must be improved in the future.
+        Also, check if the placeholder named 'keras_learning_phase' exists in the graph. This is used by Keras
+         and needs to be passed in feed_dict.
+        :return:
+        """
+        g = tf.get_default_graph()
+        for op in g.get_operations():
+            if len(op.inputs) > 0 and not op.name.startswith('gradients'):
+                if op.type in UNSUPPORTED_ACTIVATIONS:
+                    warnings.warn('Detected unsupported activation (%s). '
+                                  'This might lead to unexpected or wrong results.' % op.type)
+            elif 'keras_learning_phase' in op.name:
+                self.keras_phase_placeholder = op.outputs[0]
+
+
